be9156c758251b66938086478f255ad6e5fdd543
Ferenc Barta <barta.ferenc01@gmail.com>
2014-05-24 16:32:57 +0200

    JAXB loaders are now capable to load/save using streams.

--- src/main/java/hu/unideb/inf/prt/petriDish/loaders/JAXBConfigLoader.java
+++ src/main/java/hu/unideb/inf/prt/petriDish/loaders/JAXBConfigLoader.java
@@ -3,6 +3,13 @@ package hu.unideb.inf.prt.petriDish.loaders;
 import hu.unideb.inf.prt.petriDish.GameConfiguration;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -12,57 +19,93 @@ import javax.xml.bind.Unmarshaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JAXBConfigLoader implements ConfigLoader{
-	
-	static private Logger logger = LoggerFactory.getLogger(JAXBConfigLoader.class);
+public class JAXBConfigLoader implements ConfigLoader {
 
-	private File f=null;
-	
+	static private Logger logger = LoggerFactory
+			.getLogger(JAXBConfigLoader.class);
+
+	// private File f=null;
+	private InputStream istream = null;
+	private OutputStream ostream = null;
+	private File f;
+
 	/**
-	 * Constructor, the configuration will be loaded/saved to the
-	 * file described by the parameter.
-	 * @param f File to use
+	 * Constructor, the configuration will be loaded/saved to the file described
+	 * by the parameter.
+	 * 
+	 * @param f
+	 *            File to use
 	 */
-	public JAXBConfigLoader(File f)
-	{
-		this.f=f;
+	public JAXBConfigLoader(File f) {
+		this.f = f;
 	}
 
 	/**
-	 * Constructor, the configuration will be loaded/saved to the
-	 * file described by the parameter.
-	 * @param f File path of the file to use
+	 * Constructor, the configuration will be loaded/saved to the file described
+	 * by the parameter.
+	 * 
+	 * @param f
+	 *            File path of the file to use
 	 */
-	public JAXBConfigLoader(String path)
-	{
+	public JAXBConfigLoader(String path) {
 		f = new File(path);
 	}
+
+	public JAXBConfigLoader(InputStream istream) {
+		this.istream = istream;
+		this.ostream = null;
+	}
+
+	public JAXBConfigLoader(OutputStream ostream) {
+		this.ostream = ostream;
+		this.istream = null;
+	}
+
 	public GameConfiguration load() {
 		try {
-			JAXBContext context = JAXBContext.newInstance(GameConfiguration.class);
+			if (istream == null) {
+				istream = new FileInputStream(f);
+			}
+			JAXBContext context = JAXBContext
+					.newInstance(GameConfiguration.class);
 			Unmarshaller umarsh = context.createUnmarshaller();
-			GameConfiguration ret = (GameConfiguration)umarsh.unmarshal(f);
-			logger.info("Configuration file loaded: "+f);
+			GameConfiguration ret = (GameConfiguration) umarsh
+					.unmarshal(istream);
+			logger.info("Configuration file loaded: " + f);
 			return ret;
 		} catch (JAXBException e) {
-			logger.error("Could not load configuration file "+f);
-			logger.debug("Error message is: "+e.getMessage());
+			logger.error("Could not load configuration file " + f);
+			logger.debug("Error message is: " + e.getMessage());
+			return null;
+		} catch (FileNotFoundException e) {
+			logger.error("Could not load world description file "+f+", file not found.");
 			return null;
 		}
 	}
 
 	public boolean save(GameConfiguration conf) {
+		boolean ostreamCreatedHere = false;
 		try {
-			JAXBContext context = JAXBContext.newInstance(GameConfiguration.class);
+			if (ostream == null) {
+				ostream = new FileOutputStream(f);
+				ostreamCreatedHere = true;
+			}
+			JAXBContext context = JAXBContext
+					.newInstance(GameConfiguration.class);
 			Marshaller marsh = context.createMarshaller();
 			marsh.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			marsh.marshal(conf, f);
-			logger.info("Configuration file saved: "+f);
+			marsh.marshal(conf, ostream);
 			return true;
 		} catch (JAXBException e) {
-			logger.error("Could not save configuration file "+f);
-			logger.debug("Error message is: "+e.getMessage());
+			logger.error("Could not save configuration file " + f);
+			logger.debug("Error message is: " + e.getMessage());
 			return false;
+		} catch (FileNotFoundException e) {
+			logger.error("Could create world descriptor file "+f);
+			return false;
+		} finally
+		{
+			if (ostreamCreatedHere) ostream=null;
 		}
 	}
 }
--- src/main/java/hu/unideb/inf/prt/petriDish/loaders/JAXBWDLoader.java
+++ src/main/java/hu/unideb/inf/prt/petriDish/loaders/JAXBWDLoader.java
@@ -3,6 +3,13 @@ package hu.unideb.inf.prt.petriDish.loaders;
 import hu.unideb.inf.prt.petriDish.WorldDescriptor;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -14,9 +21,11 @@ import org.slf4j.LoggerFactory;
 
 public class JAXBWDLoader implements WDLoader {
 	
-	static private Logger logger = LoggerFactory.getLogger(JAXBConfigLoader.class);
+	static private Logger logger = LoggerFactory.getLogger(JAXBWDLoader.class);
 
-	private File f=null;
+	private InputStream istream = null;
+	private OutputStream ostream = null;
+	private File f;
 	
 	/**
 	 * Constructor, the world descriptor will be loaded/saved to the
@@ -25,7 +34,7 @@ public class JAXBWDLoader implements WDLoader {
 	 */
 	public JAXBWDLoader(File f)
 	{
-		this.f=f;
+		this.f = f;
 	}
 
 	/**
@@ -37,33 +46,64 @@ public class JAXBWDLoader implements WDLoader {
 	{
 		f = new File(path);
 	}
+	
+	public JAXBWDLoader(InputStream istream) {
+		this.istream = istream;
+		this.ostream = null;
+	}
+
+	public JAXBWDLoader(OutputStream ostream) {
+		this.ostream = ostream;
+		this.istream = null;
+	}
+	
 	public WorldDescriptor load() {
+
 		try {
-			JAXBContext context = JAXBContext.newInstance(WorldDescriptor.class);
+			if (istream == null) {
+				istream = new FileInputStream(f);
+			}
+			JAXBContext context = JAXBContext
+					.newInstance(WorldDescriptor.class);
 			Unmarshaller umarsh = context.createUnmarshaller();
-			WorldDescriptor ret = (WorldDescriptor)umarsh.unmarshal(f);
-			logger.info("World description loaded: "+f);
+			WorldDescriptor ret = (WorldDescriptor) umarsh
+					.unmarshal(istream);
+			logger.info("World descriptor file loaded: " + f);
 			return ret;
 		} catch (JAXBException e) {
 			logger.error("Could not load world description file "+f);
-			logger.error("Error message is: "+e.getMessage());
+			logger.error("Error message is: "+e);
+			return null;
+		} catch (FileNotFoundException e) {
+			logger.error("Could not load world description file "+f+", file not found.");
 			return null;
 		}
 	}
 
 	public boolean save(WorldDescriptor descr) {
+		boolean ostreamCreatedHere = false;
 		try {
-			JAXBContext context = JAXBContext.newInstance(WorldDescriptor.class);
+			if (ostream == null) {
+				ostream = new FileOutputStream(f);
+				ostreamCreatedHere = true;
+			}
+			JAXBContext context = JAXBContext
+					.newInstance(WorldDescriptor.class);
 			Marshaller marsh = context.createMarshaller();
 			marsh.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			marsh.marshal(descr, f);
-			logger.info("World descriptor file saved: "+f);
+			marsh.marshal(descr, ostream);
 			return true;
 		} catch (JAXBException e) {
 			logger.error("Could not save world descriptor to "+f);
 			logger.error("Error message is: "+e.getMessage());
 			System.out.println(e.getStackTrace());
 			return false;
+		} catch (FileNotFoundException e) {
+			logger.error("Could create world descriptor file "+f);
+			return false;
+		} finally
+		{
+			if (ostreamCreatedHere) ostream=null;
 		}
 	}
 }
